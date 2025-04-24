211ce93c445b8b9de0561d0b19dc5389a64f6b1e
James <90709035+Jamiee11@users.noreply.github.com>
2025-04-24 17:41:48 +0530

    Update SimpleXmlValidator.java

--- src/SimpleXmlValidator.java
+++ src/SimpleXmlValidator.java
@@ -1,10 +1,50 @@
-package xmlvalidatorlibrary;
+public class XMLValidator {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Invalid");
+            return;
+        }
 
-public class SimpleXmlValidator {
-    //Please implement this method
-    //feel free to add other classes/methods if you want
-    public static boolean determineXml(String xml)
-    {
-        throw new UnsupportedOperationException("Not Implemented");
+        String xml = args[0];
+        if (isValidXML(xml)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
+        }
+    }
+
+    private static boolean isValidXML(String xml) {
+        java.util.Stack<String> stack = new java.util.Stack<>();
+        int i = 0;
+        while (i < xml.length()) {
+            if (xml.charAt(i) == '<') {
+                int j = xml.indexOf('>', i);
+                if (j == -1) return false;
+
+                String tag = xml.substring(i + 1, j);
+
+                if (tag.length() == 0) return false;
+
+                boolean isClosing = tag.startsWith("/");
+                String tagName = isClosing ? tag.substring(1) : tag;
+
+                if (isClosing) {
+                    if (stack.isEmpty() || !stack.peek().equals(tagName)) {
+                        return false;
+                    }
+                    stack.pop();
+                } else {
+                    // Opening tag: must not contain space or '='
+                    if (tag.contains(" ") || tag.contains("=")) return false;
+                    stack.push(tagName);
+                }
+
+                i = j + 1;
+            } else {
+                i++;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
